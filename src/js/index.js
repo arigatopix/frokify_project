cc0addeab33d61bfa9bf6770c91efc8fe0e85cdf
BAS_DESKTOP <teeruch.k@gmail.com>
2019-04-11 18:02:15 +0700

    add query

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 // d0d01f65f7d300d9cdff9615ac5b23b7
 // https://www.food2fork.com/api/search
 
-async function getResult() {
+async function getResult(query) {
   const key = 'd0d01f65f7d300d9cdff9615ac5b23b7';
   const response = await fetch(
-    `https://www.food2fork.com/api/search?key=${key}&q=chicken%20breast&page=2`
+    `https://www.food2fork.com/api/search?key=${key}&q=${query}`
   );
   const data = await response.json();
   return data;
 }
 
-getResult().then(data => console.log(data));
+getResult('coffee').then(data => console.log(data));
