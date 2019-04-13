860fc8c1a04c8ff89c11c8b1ce372c416dfcf50f
arigatopix <teeruch.k@gmail.com>
2019-04-13 12:23:52 +0700

    search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,41 @@
-// d0d01f65f7d300d9cdff9615ac5b23b7
-// https://www.food2fork.com/api/search
+import Search from './models/Search';
+// ไม่มี { Search } เพราะ export default
 
-async function getResult(query) {
-  const key = 'd0d01f65f7d300d9cdff9615ac5b23b7';
-  const response = await fetch(
-    `https://www.food2fork.com/api/search?key=${key}&q=${query}`
-  );
-  const data = await response.json();
-  return data;
+/**** Global state of the app ไฟล์นี้ทำหน้าที่เป็น controller ของ app
+ * คล้ายๆ react สร้าง controller เก็บ state 
+ * -Search object
+ * -Current recipes object
+ * -Shopping list object
+ * -Like recipes
+*/
+  
+const state = {
+  // init state
 }
 
-getResult('coffee').then(data => console.log(data));
+const controlSearch = async() => {
+  // 1. Get query from view
+  const query = 'pizza'; // TODO รับค่าจาก UI
+
+  if(query) {
+    // 2. New search object and add to state เอา value ไป get data จาก api
+    state.search = new Search(query);
+
+    // 3. Prepare UI for results ช่องรอข้อมูลจะใส่หมุนๆ
+
+    // 4. Search for recipes (fetch from models)
+    await state.search.getResults();
+    // ใส่ await เพราะว่าข้อ 5 ดึงข้อมูลออกมา จะดึงออกมาเฉยๆ ไม่ได้ (เอา this.result มาแสดง)
+  
+    // 5. Render result from UI
+    console.log(state.search.result);
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+
+  // call state after search submit
+  controlSearch();
+
+  e.preventDefault();
+})
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,25 @@
-export default 'I am exported string.';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+
+  async getResults() {
+    const key = 'd0d01f65f7d300d9cdff9615ac5b23b7';
+    
+    try {
+      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+  
+      this.result = res.data.recipes;
+      // api ที่ส่งมา ข้อมูลอยู่ใน  data > recipes
+  
+      // console.log(this.result);
+
+    } catch (error) {
+      alert(error);
+    }
+  }
+};
+
